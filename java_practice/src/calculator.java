3a91f6212ec1194a4ff576900b2c5ca1d61be5a8
sethukarthik <m.sethukarthik@gmail.com>
2021-06-07 22:32:32 +0530

    Calculator

---
+++ java_practice/src/calculator.java
@@ -0,0 +1,25 @@
+public abstract class calculator {
+	public static void main(String[] args) {
+		//Getting input from argument
+		if(args.length == 2) {
+			try {
+				int input_1 = Integer.parseInt(args[0]);
+				int input_2 = Integer.parseInt(args[1]);
+				// Addition
+				System.out.println("I added you " + (input_1 + input_2));
+				// Subtraction
+				System.out.println("I subtracted you " + (input_1 - input_2));
+				// Multiplication
+				System.out.println("I multipled you " + (input_1 * input_2));
+				// Division
+				System.out.println("I divided you " + (input_1 / input_2));
+				// Modulo
+				System.out.println("I Remainder of you " + (input_1 % input_2));
+			}catch(NumberFormatException ex) {
+				System.out.println("I do not support characters");
+			}
+		} else {
+			System.out.println("Oops! I will accept only 2 digits");
+		}
+	}
+}
