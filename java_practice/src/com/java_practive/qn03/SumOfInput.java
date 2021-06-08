339d6dc5fcd2370c6a1c4ca3f4fa652b4b30f8e2
sethukarthik <m.sethukarthik@gmail.com>
2021-06-08 13:32:05 +0530

    Question number 3

---
+++ java_practice/src/com/java_practive/qn03/SumOfInput.java
@@ -0,0 +1,18 @@
+package com.java_practive.qn03;
+
+public class SumOfInput {
+	public static void main(String[] args) {
+		int[] intArray = {19,13,12, 0, 1, 34, -1};
+		int min_val = intArray[0];
+		int max_val = intArray[0];		
+		for(int i=1; i < intArray.length; i++) {
+			if(min_val < intArray[i]) {
+				min_val  = intArray[i];
+			}
+			if(max_val > intArray[i]) {
+				max_val = intArray[i];
+			}
+		}
+		System.out.println("The sum of Min and Max value is: " + (min_val + max_val));
+	}
+}
