b29ac56c980a521e8e1730c027fd8a4ad4a0a1d7
sethukarthik <m.sethukarthik@gmail.com>
2021-06-08 12:57:33 +0530

    Question number 3

--- java_practice/src/com/java_practice/qn02/FindVowels.java
+++ java_practice/src/com/java_practice/qn02/FindVowels.java
@@ -1,5 +1,22 @@
 package com.java_practice.qn02;
 
 public class FindVowels {
+	public static void main(String[] args) {
+		int no_of_vowels = 0;
+		
+		String input_characters = args[0];
+		input_characters = input_characters.toLowerCase();
 
+		for(int i=0; i < input_characters.length(); i++) {
+			if(input_characters.charAt(i) == 'a' || input_characters.charAt(i) == 'e' || input_characters.charAt(i) == 'i' || input_characters.charAt(i) == 'o' || input_characters.charAt(i) == 'u') {
+				no_of_vowels ++;
+			}
+		}
+		
+		if(no_of_vowels == 0) {
+			System.out.println("There is not vowels in the input");
+		}else {
+			System.out.println("The number of vowels in the inputs are: " + no_of_vowels);
+		}
+	}
 }
