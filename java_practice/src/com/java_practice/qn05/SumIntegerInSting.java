116a07b1af7e96065bed46c3fa77abb9e9a3a3c6
sethukarthik <m.sethukarthik@gmail.com>
2021-06-09 08:27:00 +0530

    Question 05 completed

--- java_practice/src/com/java_practice/qn05/SumIntegerInSting.java
+++ java_practice/src/com/java_practice/qn05/SumIntegerInSting.java
@@ -3,14 +3,29 @@ package com.java_practice.qn05;
 public class SumIntegerInSting {
 	public static void main(String[] args) {
 		String[] str = {"1AA", "14", "B12", "ABC"};
-		// Initialize an array [] for integer
-		int arrayVal[] = new int[Integer.MAX_VALUE];
 		// Remove character from the string
 		// Add and store the value in a variable and print after the loop
+		int sum_of = 0;
 		for(int i=0; i < str.length; i++) {
 			String val = str[i].replaceAll("([A-Z])", "");
 			if(!val.isEmpty()) {
+				if(val.length() == 1) {
+					int int_val = Integer.parseInt(val);
+					sum_of += int_val;
+				}
+				else{
+					String[] values = val.split("");
+					for(int j=0; j < values.length; j++) {
+						int sum_val = Integer.parseInt(values[j]);
+						sum_of += sum_val;
+					}
+				}
 			}
 		}
+		System.out.println("The sum of integers in strings are: " + sum_of);
 	}
 }
+
+
+//ToDo: Since the method using nested loop this won't look good time complexity.
+//ToDo: Need to know how method worked write the Integer parse in a seperate method and call the method
