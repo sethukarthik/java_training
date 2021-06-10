b72b6ae202a028c1d5566f50b3bc85e0140e9648
sethukarthik <m.sethukarthik@gmail.com>
2021-06-10 18:28:13 +0530

    Question 10 completed but need to resolve array allocation

--- java_practice/src/com/java_practice/qn10/Union.java
+++ java_practice/src/com/java_practice/qn10/Union.java
@@ -4,33 +4,35 @@ import java.util.Arrays;
 
 public class Union {
 	public static void main(String[] args) {
-		int input_a[] = {1,2,3,4,5};
-		int input_b[] = {3,4,6,7};
+		int input_a[] = {1,2,4,5,11,0,5,5,5,6};
+		int input_b[] = {3,4,6,9,8,8,8};
 		
-		int[] conc_array = new int[input_a.length + input_b.length];
+		int[] conc = new int[input_a.length + input_b.length];
 		for(int i=0; i < input_a.length; i++) {
-			conc_array[i] = input_a[i];
+			conc[i] = input_a[i];
 		}
 		for(int i=0; i < input_b.length; i++) {
-			conc_array[input_a.length + i] = input_b[i];
+			conc[input_a.length + i] = input_b[i];
 		}
-		Arrays.sort(conc_array);
+		Arrays.sort(conc);
+				
+		int[] array = new int[conc.length];
+		int count = 0;
+		for(int i=0; i < conc.length; i++) {
+			if(conc.length - 1 == i) {
+				array[count++] = conc[i];
+				break;
+			}
+			if(conc[i] != conc[i+1]) {
+				array[count++] = conc[i];
+			}
+		}
 		
-//		int[] array = new int[conc_array.length];
-//		int count = 0;
-//		for(int i=0; i < conc_array.length-1; i++) {
-//			if(conc_array[i] != conc_array[i+1]) {
-//				array[count++] = conc_array[i];
-//			}
-//		}
-//		array[count++] = conc_array[conc_array.length-1];
-//		
-//		for (int i=0; i<count; i++){  
-//			conc_array[i] = array[i];  
-//        }
-//		
-//		for(int i=0; i < conc_array.length; i++) {
-//			System.out.println(conc_array[i]);
-//		}
+		// remove array with value of zero		
+		for(int i=0; i < array.length; i++) {
+			if(i > 0 && array[i] != 0) {
+				System.out.println(array[i]);
+			}
+		}
 	}
 }
