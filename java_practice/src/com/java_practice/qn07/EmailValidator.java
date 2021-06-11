05417c16b09f6859e6c2d1e6482cc9b73ee22b73
sethukarthik <m.sethukarthik@gmail.com>
2021-06-11 23:35:05 +0530

    Question 07 bug fixes

--- java_practice/src/com/java_practice/qn07/EmailValidator.java
+++ java_practice/src/com/java_practice/qn07/EmailValidator.java
@@ -4,7 +4,6 @@ public class EmailValidator {
 	public static void main(String[] args) {
 		String email = "sethu@gmail.com";
 		boolean com = false;
-		boolean domain = false;
 		boolean addr_length = false;
 		boolean mail_id = false;
 		
@@ -16,36 +15,30 @@ public class EmailValidator {
 		}
 		if(com) {
 			//validate mail domain address
-			String[] domain_id = email.split("@");
-			if(domain_id.length == 2) {
-				domain = true;
-				if(domain_id[0].length() > 3) {
+			int start = 0;
+			int end = 0;
+			int ad_count = 0;
+			int dom_count = 0;
+			for(int i=0; i < email.length(); i++) {
+				char c= email.charAt(i);
+				if(c == '@') {
+					ad_count ++;
+					start = email.indexOf(c);
+				}else if(c == '.') {
+					dom_count ++;
+					end = email.indexOf(c);
+				}
+			}
+			if(ad_count == 1 && dom_count == 1) {
+				if((end - start) >= 4) {
+					mail_id = true;
+				}
+				if(start >= 4) {
 					addr_length = true;
 				}
 			}
-			if(domain && addr_length) {
-				int count = 0;
-				for(int i=0; i < email.length(); i++) {
-					char c = email.charAt(i);
-					if(c == '@') {
-						count = 0;
-					}
-					System.out.println(c);
-					System.out.println(count);
-					if(count == 4 && c != '.') {
-						mail_id = true;
-						break;
-					}
-					count += 1;
-				}
-				
-			}
 		}
-		System.out.println(com);
-		System.out.println(domain);
-		System.out.println(addr_length);
-		System.out.println(mail_id);
-		if(com && domain && addr_length && mail_id) {
+		if(com && mail_id) {
 			System.out.println(email + " Hurrey! is a valid address.");
 		}else {
 			System.out.println(email + " Oops! please check your mail address.");
