e5f7ce00d2bea6bc9cfa6ea8622170ae6215d9b7
SethuNagaKarthik <m.sethukarthik@gmail.com>
2021-07-03 09:18:16 +0530

    Adding after classpath resolved

---
+++ java_practice/com/java_practice/testing/Test.java
@@ -0,0 +1,7 @@
+package com.java_practice.testing;
+
+public class Test {
+	private void name() {
+		System.out.println("Comin");
+	}
+}
--- java_practice/src/HelloWorld.java
+++ java_practice/src/HelloWorld.java
@@ -1,6 +1,6 @@
 
 public abstract class HelloWorld {
- public static void main(String[] args) {
-	 System.out.println("Hello World");
- }
+	 public static void main(String[] args) {
+		 System.out.println("Hello World");
+	 }
 }
